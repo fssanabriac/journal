9ca22e290d42169d37a702a83d145b3ac130ab0d
Sebastian Sanabria <fssanabriac@gmail.com>
2022-02-14 21:34:50 -0500

    Add dark/light icons

--- scripts/darkMode.js
+++ scripts/darkMode.js
@@ -1,18 +1,26 @@
 
 let darkMode = false;
+const darkColor = 'grey';
+const lightColor = 'white';
 
 changeMode = () => {
-    if (!darkMode){
-        document.
-            getElementById('darkModeButton').
-            innerHTML = 'Light';
-        document.body.style.backgroundColor = 'grey';
-        darkMode = !darkMode;
-    }else{
-        document.
-            getElementById('darkModeButton').
-            innerHTML = 'Dark';
-        document.body.style.backgroundColor = 'white';
-        darkMode = !darkMode;
-    }
+  if (!darkMode){
+    document.
+      getElementById('darkModeButton').
+      innerHTML = '<i class="fa-solid fa-sun fa-2x"></i>';
+    document.
+      getElementById('darkModeButton').
+      style.backgroundColor = darkColor;
+      document.body.style.backgroundColor = darkColor;
+    darkMode = !darkMode;
+  }else{
+    document.
+      getElementById('darkModeButton').
+      innerHTML = '<i class="fa-solid fa-moon fa-2x"></i>' ;
+    document.
+      getElementById('darkModeButton').
+      style.backgroundColor = lightColor;
+    document.body.style.backgroundColor = lightColor;
+    darkMode = !darkMode;
+  }
 }
